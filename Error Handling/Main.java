e06d0a26801aeb237b3aa1c80e13a9cad0779a12
leofzj <1518498462@qq.com>
2024-03-19 17:18:52 +0800

    4

--- Error Handling/Main.java
+++ Error Handling/Main.java
@@ -1,11 +1,16 @@
 package dividebyzero;
+
 public class Main {
- 
- /**
- * @param args the command line arguments
- */
- public static void main(String[] args) {
- System.out.println(3/0);
-}
- 
+  /**
+	 * @param args the command line arguments
+	 */
+    public static void main(String[] args) {
+        try {
+            System.out.println(3/0);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Caught runtime exception \r\n" + "= %s\\n");
+        } 
+   }
 }
